c720a9531bd71ad49b16cf81c57741fb9bc6c86b
Pedro Henrique A. Basilio Dos Santos <103258801+PhBasilio07@users.noreply.github.com>
2022-06-09 19:24:05 -0300

    Exercicio 4 - Lista Herança

---
+++ Parte3/TesteInstancia.java
@@ -0,0 +1,10 @@
+package Parte3;
+
+public class TesteInstancia {
+    public static void main(String[] args) {
+        Cachorro c = new Cachorro("Nina", 4);
+        Gato g = new Gato("Dunga", 4);
+        System.out.println(c);
+        System.out.println(g);
+    }
+}
