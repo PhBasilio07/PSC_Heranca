bc291fe64836b243311e094e503c25621350afbf
Pedro Henrique A. Basilio Dos Santos <103258801+PhBasilio07@users.noreply.github.com>
2022-06-10 09:06:47 -0300

    Exercicio 3 - Lista Herança

--- Parte3/Cachorro.java
+++ Parte3/Cachorro.java
@@ -2,7 +2,7 @@ package Parte3;
 
 public class Cachorro extends Mamifero{
 
-    public Cachorro(String nome, int patas) {
-        super(nome, patas);
+    public Cachorro(String nome) {
+        super(nome, 4);
     }
 }
